b78399fb84836138ce61698780fd84bcfadc1d53
Anton Popov <ademaro@ya.ru>
2022-03-26 07:12:53 +0530

    Flask-resize version updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [3.1.0] - 2022-03-26
+### Added
+ - Checking the modification date of the source file
+ - Crop to no fill images
+
 ## [3.0.1] - 2021-07-10
 ### Added
  - Support WebP format
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ pkgname = appname.lower().replace("-", "_")
 
 setup(
     name=appname,
-    version="3.0.1",
+    version="3.1.0",
     description="Flask extension for resizing images in code and templates",
     long_description=__doc__,
     packages=find_packages(),
